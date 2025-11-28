5ab420f72f3a8eb7fa16a67b6d3a8defa42f29dc
Mohammed Shahal <shahalt64@gmail.com>
2025-11-29 00:28:29 +0530

    test file

--- dist/models/Product.js
+++ dist/models/Product.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 
 const productSchema = new Schema(
   {
-    name: { type: String, required: true, trim: true, unique: true },
+    name: { type: String, required: true, trim: true },
     description: { type: String, trim: true },
     price: { type: Number, min: 0 },
 
